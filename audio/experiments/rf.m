875af778d84ae0e6378003825bb61c55853a3f0c
Shi Hu <s3hu@corn11.stanford.edu>
2014-06-01 00:48:21 -0700

    remove old random forest

--- audio/experiments/rf.m
+++
@@ -1,9 +0,0 @@
-% random forest
-
-traindata = csvread('age.train');
-testdata = csvread('age.test');
-
-numtrees = 20;
-b = TreeBagger(numtrees, traindata(:,2:end), traindata(:,1), 'Method', 'classification');
-preds = str2double(b.predict(testdata(:,2:end)));
-acc = sum(preds == testdata(:,1))/numel(preds);
