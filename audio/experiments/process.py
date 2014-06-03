b47dadad1fb35f6e186767d6aafb4e1337a7e372
Shi Hu <s3hu@corn02.stanford.edu>
2014-06-02 17:24:16 -0700

    add reformat script

--- audio/experiments/process.py
+++ audio/experiments/process.py
@@ -1,35 +1,25 @@
 import sys,os
 
-prop = 'gender'
+prop = sys.argv[1]
 train = open('../label_feats/train_' + prop + '.lsvm','r')
+valid = open('../label_feats/valid_' + prop + '.lsvm','r')
 test = open('../label_feats/test_' + prop + '.lsvm','r')
 
-f = open(prop + '.train','w')
+def dowork(filename, fhandle):
+  f = open(filename,'w')
+  for line in fhandle:
+    parts = line.split()
+    new_line = parts[0] + ','
+    parts.pop(0)
+    for i,p in enumerate(parts):
+      val = p.split(':')[1].strip()
+      if i < len(parts) - 1:
+	new_line += val + ','
+      else:
+	new_line += val + '\n'
+    f.write(new_line)
+  f.close()
 
-for line in train:
-  parts = line.split()
-  new_line = parts[0] + ','
-  parts.pop(0)
-  for i,p in enumerate(parts):
-    val = p.split(':')[1].strip()
-    if i < len(parts) - 1:
-      new_line += val + ','
-    else:
-      new_line += val + '\n'
-  f.write(new_line)
-
-g = open(prop + '.test','w')
-for line in test:
-  parts = line.split()
-  new_line = parts[0] + ','
-  parts.pop(0)
-  for i,p in enumerate(parts):
-    val = p.split(':')[1].strip()
-    if i < len(parts) - 1:
-      new_line += val + ','
-    else:
-      new_line += val + '\n'
-  g.write(new_line)
-
-f.close()
-g.close()
+dowork(prop + '.train', train)
+dowork(prop + '.valid', valid)
+dowork(prop + '.test', test)
